2d5d797e2d813494d878fe0426ffcd89bf465310
duvetofwisdom <duvetofwisdom@proton.me>
2024-01-20 12:14:37 +0100

    Adding the title prop to the Default layout

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -1,4 +1,13 @@
 /**
+ * Base title for pages in the application.
+ */
+export const PAGE_TITLE_BASE = 'RiceField';
+/**
+ * String used to separate different parts of a page title.
+ */
+export const PAGE_TITLE_SPLITTER = ' | ';
+
+/**
  * Enumeration representing different routes in the application.
  * @enum {string}
  */
