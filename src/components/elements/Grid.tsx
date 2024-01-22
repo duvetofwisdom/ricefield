df3255ffed3c24a652df6a00699f6f5bbace5984
duvetofwisdom <duvetofwisdom@proton.me>
2024-01-22 09:31:43 +0100

    Handling active grid boxes state

--- src/components/elements/Grid.tsx
+++ src/components/elements/Grid.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
+import type { Box } from '@typings/box';
+
 interface Props {
   width: number;
   height: number;
+  activeBoxes: Box[];
+  onBoxClick: (box: Box) => void;
 }
 
-export const Grid: React.FC<Props> = ({ width, height }) => (
+export const Grid: React.FC<Props> = ({
+  width,
+  height,
+  activeBoxes,
+  onBoxClick,
+}) => (
   <div className="rf-grid">
-    {Array.from({ length: height }, (_, index) => (
-      <div key={index} className="rf-grid__row">
-        {Array.from({ length: width }, (_, index) => (
-          <div key={index} className="rf-grid__cell" />
+    {Array.from({ length: height }, (_, rowIndex) => (
+      <div key={rowIndex} className="rf-grid__row">
+        {Array.from({ length: width }, (_, colIndex) => (
+          <div
+            key={colIndex}
+            onClick={() =>
+              onBoxClick({
+                x: colIndex,
+                y: rowIndex,
+              })
+            }
+            className={`rf-grid__cell${activeBoxes.find((box) => box.x === colIndex && box.y === rowIndex) ? ' rf-grid__cell--active' : ''}`}
+          />
         ))}
       </div>
     ))}
--- src/pages/ShellPage.tsx
+++ src/pages/ShellPage.tsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { DefaultLayout } from '@layouts/DefaultLayout';
 
 import { Grid } from '@components/elements/Grid';
 
-export const ShellPage: React.FC = () => (
-  <DefaultLayout title="Shell">
-    <h1>Shell</h1>
-    <h2>Splash Art</h2>
-    <Grid width={6} height={4} />
-  </DefaultLayout>
-);
+import type { ChangeEvent } from 'react';
+
+import type { Box } from '@typings/box';
+
+export const ShellPage: React.FC = () => {
+  const [activeBoxes, setActiveBoxes] = useState<Box[]>([]);
+  const [inputWidth, setInputWidth] = useState<number>(4);
+  const [inputHeight, setInputHeight] = useState<number>(3);
+  const [width, setWidth] = useState<number>(inputWidth);
+  const [height, setHeight] = useState<number>(inputHeight);
+
+  const handleResize = () => {
+    setWidth(inputWidth);
+    setHeight(inputHeight);
+  };
+
+  const handleBoxClick = (box: Box) => {
+    const existingIndex = activeBoxes.findIndex(
+      (activeBox) => activeBox.x === box.x && activeBox.y === box.y
+    );
+    existingIndex !== -1
+      ? setActiveBoxes(
+          activeBoxes.filter((_, index) => index !== existingIndex)
+        )
+      : setActiveBoxes((prev) => [...prev, box]);
+  };
+
+  return (
+    <DefaultLayout title="Shell">
+      <h1>Shell</h1>
+      <h2>Splash Art</h2>
+      <label htmlFor="width">Width:</label>
+      <br />
+      <input
+        type="number"
+        name="width"
+        value={inputWidth}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setInputWidth(Number(e.target.value))
+        }
+      />
+      <br />
+      <label htmlFor="height">Height:</label>
+      <br />
+      <input
+        type="number"
+        name="height"
+        value={inputHeight}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setInputHeight(Number(e.target.value))
+        }
+      />
+      <br />
+      <button onClick={handleResize}>Resize</button>
+      <Grid
+        width={width}
+        height={height}
+        activeBoxes={activeBoxes}
+        onBoxClick={handleBoxClick}
+      />
+    </DefaultLayout>
+  );
+};
